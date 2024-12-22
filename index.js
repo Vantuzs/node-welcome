c9c5b2a5906d88894fbc8815a07d2b13e2929e30
Vantuz <Shcherbina444@gmail.com>
2024-12-22 17:26:30 +0200

    readFile

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 'use strict';
 
-/* 
+//fs - file sistem
 
-require(path) 
+const fs = require('fs').promises;
 
-Как работает require(HOW REQUIRE WORKS)
-
-resolving: ищет файл который мы указали -> 
-loading: читает файл, когда файл прочитано -> 
-wrappening: оборачивание в node контекст -> 
-evaluation: код выполнется -> 
-caching: кешируется результат, сохраняет результат роботы нашего модуля, код в середине модуля выполнится 1 раз
-
-RESOLVING:
-    1) Core modules(встроеные модули)
-    2) File
-        *.js|| *.json
-    3)
-        3.1) package.json -> "main"
-        3.2) index.js || index.json
-    4) node_modules
-    5) throw new Error();
-
-*/
+const p = fs.readFile('./text.txt', 'utf8');
+p.then(data=>console.log(data));
