d32b28126ba39ec036a202f1ef92b46f13a31afc
Vantuz <Shcherbina444@gmail.com>
2024-12-22 18:01:54 +0200

    appendFile

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const fs = require('fs').promises;
 
 fs.readFile('./text.txt', 'utf8')
 .then(content=>{
-    const newContent = `12 OLD content: ${content} and NEW content: 'Hello world, BLYA'`
-    fs.writeFile('./newFile.txt',newContent,'utf8'); // Перезаписывает файлы
+    const newContent = `APPENDs OLD content: ${content} and NEW content: 'Hello world, BLYA'`
+    // fs.writeFile('./newFile.txt',newContent,'utf8'); // Перезаписывает файлы!!!
+    fs.appendFile('./newFile.txt',newContent,'utf8'); // Дописывает файлы
 });
 
+/* 
+
+Задача: заминить какуюто часть файла
+
+1. Читаем ВЕСЬ файл через readFile -> ВЕСЬ вмист файлу
+2. Както меняем содержимое через JS ->
+
+
+*/
